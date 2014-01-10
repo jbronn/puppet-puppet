f3c2f682102a0708126cd5bbe5e1fa3f19da27d8
Justin Bronn <justin@counsyl.com>
2014-01-09 22:07:07 -0800

    Can now configure the Puppet Labs apt repository.

---
+++ manifests/apt.pp
@@ -0,0 +1,33 @@
+# == Classs: puppet::apt
+#
+# Installs the GPG key and configures the source list for packages
+# from Puppet Labs.
+#
+class puppet::apt(
+  $url          = 'https://apt.puppetlabs.com/',
+  $distribution = [$::lsbdistcodename],
+  $components   = ['main'],
+  $gpg_source   = 'puppet:///puppet/apt/puppetlabs.gpg',
+) {
+  include sys::apt::update
+
+  sys::apt::key { $gpg_source:
+    ensure => present,
+  }
+
+  $sources = "${sys::apt::sources_d}/puppetlabs.list"
+  sys::apt::sources { $sources:
+    repositories => [{
+      'uri'          => $url,
+      'distribution' => $distribution,
+      'components'   => $components,
+    }],
+    source       => false,
+    require      => Sys::Apt::Key[$gpg_source],
+    notify       => Class['sys::apt::update'],
+  }
+
+  anchor { 'counsyl::ubuntu::pgdg::apt':
+    require => Class['sys::apt::update'],
+  }
+}
