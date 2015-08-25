9556ec8a1273f0a41ed66030e3c7e82a5f446cdf
Justin Bronn <justin@counsyl.com>
2015-08-25 12:58:42 -0500

    Add `puppet::r10k::config` defined type.

---
+++ manifests/r10k/config.pp
@@ -0,0 +1,57 @@
+# == Define: puppet::r10k::config
+#
+# Generates a
+#
+# === Parameters
+#
+# [*cachedir*]
+#  The path to use for the r10k cache directory, false by default.
+#
+# [*forge*]
+# 
+# [*owner*]
+#  Owner of r10k config file, undefined by default.
+#
+# [*group*]
+#  Group of r10k config file, undefined by default.
+#
+# [*mode*]
+#  Mode of r10k config file, defaults to '0600'.
+#
+# [*source*]
+#  Advanced usage only, specifies the puppet file source for the r10k
+#  configuration file; undefined by default.
+#
+# [*template*]
+#  Advanced usage only, the template used to generate the r10k
+#  configuration file; defaults to 'puppet/r10k/r10k.yaml.erb'.
+#
+define puppet::r10k::config(
+  $cachedir = false,
+  $forge    = {},
+  $git      = {},
+  $sources  = {},
+  $postrun  = [],
+  $owner    = undef,
+  $group    = undef,
+  $mode     = '0600',
+  $source   = undef,
+  $template = 'puppet/r10k/r10k.yaml.erb',
+) {
+  validate_absolute_path($title)
+  validate_array($postrun)
+  validate_hash($forge, $git, $sources)
+
+  if is_string($cachedir) {
+    validate_absolute_path($cachedir)
+  }
+
+  file { $title:
+    ensure  => file,
+    owner   => $owner,
+    group   => $group,
+    mode    => $mode,
+    source  => $source,
+    content => template($template),
+  }
+}
