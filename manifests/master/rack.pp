cc60d2a43bd92df344156d32d5f29a465e5ec30b
Justin Bronn <justin@counsyl.com>
2015-08-25 11:23:06 -0500

    Fix improper indentation linting errors.

--- manifests/master/rack.pp
+++ manifests/master/rack.pp
@@ -6,32 +6,32 @@
 class puppet::master::rack {
   $conf = "${puppet::master::confdir}/rack"
   file { $conf:
-    ensure  => directory,
-    owner   => 'root',
-    group   => $puppet::master::group,
-    mode    => '0640',
+    ensure => directory,
+    owner  => 'root',
+    group  => $puppet::master::group,
+    mode   => '0640',
   }
 
   $puppetmaster = "${conf}/puppetmaster"
   file { $puppetmaster:
-    ensure  => directory,
-    owner   => 'root',
-    group   => $puppet::master::group,
-    mode    => '0640',
+    ensure => directory,
+    owner  => 'root',
+    group  => $puppet::master::group,
+    mode   => '0640',
   }
 
   file { "${puppetmaster}/public":
-    ensure  => directory,
-    owner   => 'root',
-    group   => $puppet::master::group,
-    mode    => '0640',
+    ensure => directory,
+    owner  => 'root',
+    group  => $puppet::master::group,
+    mode   => '0640',
   }
 
   file { "${puppetmaster}/tmp":
-    ensure  => directory,
-    owner   => 'root',
-    group   => $puppet::master::group,
-    mode    => '0640',
+    ensure => directory,
+    owner  => 'root',
+    group  => $puppet::master::group,
+    mode   => '0640',
   }
 
   file { "${puppetmaster}/config.ru":
