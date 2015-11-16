bb0f7a17b5d15177aa7b09bfea62a08238c684f7
Robin Walsh <robin@counsyl.com>
2015-11-16 14:12:41 -0800

    adding support for merge behavior

--- manifests/hiera_config.pp
+++ manifests/hiera_config.pp
@@ -15,6 +15,9 @@
 #  Array of strings, where each string is the name of a static or dynamic
 #  data source.
 #
+# [*merge_behavior*]
+#  Merge behavior to use. 'native' by default.
+#
 # [*logger*]
 #  The logger for hiera to use, undefined by default.
 #
@@ -31,15 +34,16 @@
 #  Advanced usage only, defaults to 'puppet/hiera.yaml.erb'.
 #
 define puppet::hiera_config(
-  $backends  = ['yaml'],
-  $settings  = {},
-  $hierarchy = [],
-  $logger    = undef,
-  $owner     = undef,
-  $group     = undef,
-  $mode      = '0600',
-  $source    = undef,
-  $template  = 'puppet/hiera.yaml.erb',
+  $backends       = ['yaml'],
+  $settings       = {},
+  $hierarchy      = [],
+  $merge_behavior = 'native',
+  $logger         = undef,
+  $owner          = undef,
+  $group          = undef,
+  $mode           = '0600',
+  $source         = undef,
+  $template       = 'puppet/hiera.yaml.erb',
 ){
   validate_absolute_path($title)
   validate_array($backends, $hierarchy)
