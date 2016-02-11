9a97afbed64980ddde31197edc4ca3983c7ada00
Justin Bronn <justin@counsyl.com>
2016-02-11 13:50:03 -0800

    Fix outstanding issues with apt-based packaging.

--- manifests/install/apt.pp
+++ manifests/install/apt.pp
@@ -9,10 +9,12 @@ class puppet::install::apt(
   $suffix         = '1puppetlabs1'
 ) {
   if $::osfamily != 'Debian' {
-    fail("Can only install Puppet with apt on Debian platforms.\n")
+    fail('Can only install Puppet with apt on Debian platforms.')
   }
 
   if $puppet::facter_version =~ /-/ {
+    $facter_version = $puppet::facter_version
+  } elsif $puppet::facter_version =~ /^(absent|installed|present|uninstalled)$/ {
     $facter_version = $puppet::facter_version
   } else {
     $facter_version = "${puppet::facter_version}-${suffix}"
@@ -20,11 +22,15 @@ class puppet::install::apt(
 
   if $puppet::hiera_version =~ /-/ {
     $hiera_version = $puppet::hiera_version
+  } elsif $puppet::hiera_version =~ /^(absent|installed|present|uninstalled)$/ {
+    $hiera_version = $puppet::hiera_version
   } else {
     $hiera_version = "${puppet::hiera_version}-${suffix}"
   }
 
   if $puppet::version =~ /-/ {
+    $puppet_version = $puppet::version
+  } elsif $puppet::version =~ /^(absent|installed|present|uninstalled)$/ {
     $puppet_version = $puppet::version
   } else {
     $puppet_version = "${puppet::version}-${suffix}"
@@ -32,23 +38,10 @@ class puppet::install::apt(
 
   include puppet::apt
 
-  package { $facter_package:
-    ensure  => $facter_version,
-    require => Class['puppet::apt'],
-  }
-
-  package { $hiera_package:
-    ensure  => $hiera_version,
-    require => Class['puppet::apt'],
-  }
-
-  package { $common_package:
-    ensure  => $puppet_version,
-    require => Package[$facter_package, $hiera_package],
-  }
-
-  package { $puppet::package:
-    ensure  => $puppet_version,
-    require => Package[$common_package],
-  }
+  ensure_packages([$facter_package], { 'ensure' => $facter_version })
+  ensure_packages([$hiera_package], { 'ensure' => $hiera_version })
+  ensure_packages(
+    [$common_package, $puppet_package],
+    { 'ensure' => $puppet_version }
+  )
 }
